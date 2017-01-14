6da98e67ae1c1a41b4d8351b13095b3275d6f74d
brunogabriel <bruno.gsantos89@gmail.com>
2017-01-14 12:58:25 -0200

    Starting sample

---
+++ AndroidRealmNotes/app/src/main/java/brunogabriel/github/io/androidrealmnotes/manager/BaseActivity.java
@@ -0,0 +1,11 @@
+package brunogabriel.github.io.androidrealmnotes.manager;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by brunogabriel on 14/01/17.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
---
+++ AndroidRealmNotes/app/src/main/java/brunogabriel/github/io/androidrealmnotes/views/activities/LoginActivity.java
@@ -0,0 +1,11 @@
+package brunogabriel.github.io.androidrealmnotes.views.activities;
+
+import brunogabriel.github.io.androidrealmnotes.manager.BaseActivity;
+
+/**
+ * Created by brunogabriel on 14/01/17.
+ */
+
+public class LoginActivity extends BaseActivity {
+
+}
