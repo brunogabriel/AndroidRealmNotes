1dd7386a49f18e0be9cf199575ddc54c05c7959d
brunogabriel <bruno.gsantos89@gmail.com>
2017-01-14 19:11:56 -0200

    Added layout to structure in login

--- AndroidRealmNotes/app/src/main/java/brunogabriel/github/io/androidrealmnotes/manager/BaseActivity.java
+++ AndroidRealmNotes/app/src/main/java/brunogabriel/github/io/androidrealmnotes/manager/BaseActivity.java
@@ -8,4 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+    protected void initUI() {};
+
 }
--- AndroidRealmNotes/app/src/main/java/brunogabriel/github/io/androidrealmnotes/views/activities/LoginActivity.java
+++ AndroidRealmNotes/app/src/main/java/brunogabriel/github/io/androidrealmnotes/views/activities/LoginActivity.java
@@ -1,5 +1,15 @@
 package brunogabriel.github.io.androidrealmnotes.views.activities;
 
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+
+import java.util.Locale;
+
+import brunogabriel.github.io.androidrealmnotes.R;
 import brunogabriel.github.io.androidrealmnotes.manager.BaseActivity;
 
 /**
@@ -8,4 +18,26 @@ import brunogabriel.github.io.androidrealmnotes.manager.BaseActivity;
 
 public class LoginActivity extends BaseActivity {
 
+    // TODO: Use Butterknife
+    protected AppCompatTextView tvNoAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initUI();
+    }
+
+    @Override
+    protected void initUI() {
+        tvNoAccount = (AppCompatTextView) findViewById(R.id.tvNoAccount);
+
+        String noAccount = getString(R.string.app_login_activity_no_account);
+        String signup = getString(R.string.app_login_activity_signup);
+
+        SpannableString spannableString = new SpannableString(noAccount + " " + signup.toUpperCase(Locale.getDefault()));
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), spannableString.length() - signup.length(), spannableString.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+        tvNoAccount.setText(spannableString);
+    }
 }
